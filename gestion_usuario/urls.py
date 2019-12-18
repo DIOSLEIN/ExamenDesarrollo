f3946976a3b41de900eecfda15ae4a6789982f3e
manuel <man.arayah@alumnos.duoc.cl>
2019-12-18 16:07:35 -0300

    boton presupuesto solo funciona con user logeado

---
+++ gestion_usuario/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+app_name = 'gestion_usuario'
+
+urlpatterns = [
+    url('registrar/', views.registrar, name='registrar'),
+    url('login/', views.usuario_login, name='login'),
+    url('logout/', views.usuario_logout, name='logout'),
+    url('presupuesto/', views.presupuesto, name='presupuesto'),
+    url('', views.index, name='index')
+]
